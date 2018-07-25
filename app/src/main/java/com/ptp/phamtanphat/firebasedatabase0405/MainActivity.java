3e56c15da9f7c6658362856e12bf1c31502770a3
thuviengithub0208 <khoaphp@yahoo.com>
2018-07-25 17:52:53 +0700

    Initial commit

---
+++ app/src/main/java/com/ptp/phamtanphat/firebasedatabase0405/MainActivity.java
@@ -0,0 +1,58 @@
+package com.ptp.phamtanphat.firebasedatabase0405;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Chuoi : Nguyen Van A
+//        myRef.child("Hoc sinh").child("Lop 10").setValue("20 em");
+        //Integer : 20
+//         myRef.child("Si so").setValue(30);
+//        Nhanvien nhanvien1 = new Nhanvien("Nguyen Van A",25);
+//        myRef.child("Nhan vien").push().setValue(nhanvien1);
+
+//        myRef.child("Hocsinh").addValueEventListener(new ValueEventListener() {
+//        @Override
+//        public void onDataChange(DataSnapshot dataSnapshot) {
+//            String value = (String) dataSnapshot.getValue();
+//            Toast.makeText(MainActivity.this, value, Toast.LENGTH_SHORT).show();
+//        }
+//
+//        @Override
+//        public void onCancelled(DatabaseError databaseError) {
+//            Log.d("BBB",databaseError.getMessage());
+//        }
+//    });
+        Nhanvien nhanvien = new Nhanvien("Nguyen Van A",20);
+        Nhanvien nhanvien1 = new Nhanvien("Nguyen Van B",25);
+        Nhanvien nhanvien2 = new Nhanvien("Nguyen Van C",30);
+
+        myRef.child("Nhanvien").push().setValue(nhanvien2).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                Log.d("BBB",task.toString());
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/ptp/phamtanphat/firebasedatabase0405/Nhanvien.java
@@ -0,0 +1,27 @@
+package com.ptp.phamtanphat.firebasedatabase0405;
+
+public class Nhanvien {
+    private String ten;
+    private Integer tuoi;
+
+    public Nhanvien(String ten, Integer tuoi) {
+        this.ten = ten;
+        this.tuoi = tuoi;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public Integer getTuoi() {
+        return tuoi;
+    }
+
+    public void setTuoi(Integer tuoi) {
+        this.tuoi = tuoi;
+    }
+}
